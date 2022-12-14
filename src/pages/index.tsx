3348cdeae367f3ecf4ef150308185c05aa83c4aa
lamongabriel <gabriel-lamon@outlook.com>
2022-12-13 21:10:12 -0300

    global styles + eslint + cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,12 @@
-import { useRef } from "react"
-
-import styled from '../styles/home.module.scss'
+import Head from 'next/head'
 
 export default function Home() {
-
-  const inputRef = useRef<HTMLInputElement>(null)
-
-  function focus(){
-    inputRef.current?.focus()
-  }
-
-  return (
-    <>
-      <input ref={inputRef} />
-      <button className={styled.title} onClick={focus}>Focus</button>
-    </>
-  )
+	return(
+		<>
+			<Head>
+				<title></title>
+			</Head>
+			<h1>Hello World</h1>
+		</>
+	)
 }
