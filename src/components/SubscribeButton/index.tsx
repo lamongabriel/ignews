42716427d1d72967afc989858de4b72732e39156
lamongabriel <gabriel-lamon@outlook.com>
2022-12-14 10:18:59 -0300

    Subscribe button - basic component

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,7 @@
+import styles from './styles.module.scss'
+
+export function SubscribeButton () {
+	return (
+		<button className={styles.button__subscribe}>Subscribe now</button>
+	)
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { Header } from '../components/Header'
 import styles from './home.module.scss'
 
 import womanImage from '../assets/woman.svg'
+import { SubscribeButton } from '../components/SubscribeButton'
 
 export default function Home() {
 	return(
@@ -17,13 +18,15 @@ export default function Home() {
 			<main className={styles.home__content + ' container'}>
 				<section>
 					<span>👏 Hey, welcome</span>
+
 					<div>
 						<h1>News about<br/>the <strong>React</strong> world</h1>
 						<p>Get access to all the publications<br/>
 							<span><strong>for $9.90 month.</strong></span>
 						</p>
 					</div>
-					<button>Subscribe now</button>
+
+					<SubscribeButton />
 				</section>
 
 				<Image src={womanImage} alt='Drawing of a woman on a chair' />
