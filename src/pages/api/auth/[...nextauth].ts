0ecb4dd8619ee411e0f846c906a4c7412b7d0ce0
lamongabriel <gabriel-lamon@outlook.com>
2022-12-14 16:10:15 -0300

    register user email to faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,11 @@
-import NextAuth from 'next-auth'
+import { query as q } from 'faunadb'
+
+import NextAuth, { AuthOptions } from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 
-export const authOptions = {
+import { fauna } from '../../../services/fauna'
+
+export const authOptions: AuthOptions = {
 	providers: [
 		GithubProvider({
 			clientId: process.env.GITHUB_ID as string,
@@ -16,6 +20,25 @@ export const authOptions = {
 			},
 		}),
 	],
+	callbacks: {
+		async signIn({ user }) {
+			const { email } = user
+
+			try {
+				await fauna.query(
+					q.Create(
+						q.Collection('users'),
+						{
+							data: { email }
+						}
+					)
+				)
+				return true
+			} catch {
+				return false
+			}
+		},
+	}
 }
 
 export default NextAuth(authOptions)
