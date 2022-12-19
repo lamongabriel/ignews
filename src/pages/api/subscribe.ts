edf3daae0e5bdc37d32a197339a3325b326c40e1
lamongabriel <gabriel-lamon@outlook.com>
2022-12-19 11:19:40 -0300

    stripe prevent customer duplication

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,6 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from 'next-auth/react'
+import { query as q } from 'faunadb'
+
 import { stripe } from '../../services/stripe'
+import { fauna } from '../../services/fauna'
+
+interface User {
+	ref: {
+		id: string
+	}
+	data: {
+		stripe_customer_id: string
+	}
+}
 
 export default async function (req: NextApiRequest, res: NextApiResponse){
 	if(req.method === 'POST'){
@@ -11,12 +23,28 @@ export default async function (req: NextApiRequest, res: NextApiResponse){
 			return
 		}
 
-		const stripeCustomer = await stripe.customers.create({
-			email: session.user.email
-		})
+		const user = await fauna.query(q.Get(q.Match(q.Index('user_by_email'), q.Casefold(session.user.email)))) as User
+
+		let customerId = user.data.stripe_customer_id
+
+		if(!customerId){
+			const stripeCustomer = await stripe.customers.create({
+				email: session.user.email
+			})
+
+			await fauna.query(
+				q.Update(q.Ref(q.Collection('users'), user.ref.id), {
+					data: {
+						stripe_customer_id: stripeCustomer.id
+					}
+				})
+			)
+
+			customerId = stripeCustomer.id
+		}
 
 		const stripeCheckoutSession = await stripe.checkout.sessions.create({
-			customer: stripeCustomer.id,
+			customer: customerId,
 			payment_method_types: ['card'],
 			billing_address_collection: 'required',
 			line_items: [{
