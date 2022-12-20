7b3dd67197c656d9b9e1e5593262a8d93b13f230
lamongabriel <gabriel-lamon@outlook.com>
2022-12-20 16:46:17 -0300

    active link header component

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,20 @@
+import Link, { LinkProps } from 'next/link'
+import { useRouter } from 'next/router'
+
+interface ActiveLinkProps extends LinkProps{
+	children: string
+	activeClassName: string
+}
+
+export function ActiveLink ({children, activeClassName,...rest}: ActiveLinkProps) {
+
+	const { asPath } = useRouter()
+
+	const className = asPath === rest.href ? activeClassName : ''
+
+	return (
+		<Link className={className} {...rest}>
+			{children}
+		</Link>
+	)
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,26 @@
-import Link from 'next/link'
 import Image from 'next/image'
+
 import { SignInButton } from '../SignInButton'
 
 import styles from './styles.module.scss'
 
 import logo from '../../assets/logo.svg'
+import { ActiveLink } from '../ActiveLink'
 
 export function Header () {
+
 	return (
 		<header className={styles.headerContainer}>
 			<div className={styles.headerContent + ' container'}>
 				<Image src={logo} alt="ig.news logo"/>
 
 				<nav>
-					<Link href='/' className={styles.active}>
+					<ActiveLink activeClassName={styles.active} href='/'>
 						Home
-					</Link>
-					<Link href='/posts' prefetch>
+					</ActiveLink>
+					<ActiveLink activeClassName={styles.active} href='/posts' prefetch>
 						Posts
-					</Link>
+					</ActiveLink>
 				</nav>
 
 				<SignInButton />
