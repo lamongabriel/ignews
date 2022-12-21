3b6798d65f9132f19c577a6791b52a7cc4b2e941
lamongabriel <gabriel-lamon@outlook.com>
2022-12-21 16:10:28 -0300

    Home direct by logo click

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,22 @@
+import Link from 'next/link'
 import Image from 'next/image'
 
 import { SignInButton } from '../SignInButton'
+import { ActiveLink } from '../ActiveLink'
 
 import styles from './styles.module.scss'
 
 import logo from '../../assets/logo.svg'
-import { ActiveLink } from '../ActiveLink'
 
 export function Header () {
 
 	return (
 		<header className={styles.headerContainer}>
 			<div className={styles.headerContent + ' container'}>
-				<Image src={logo} alt="ig.news logo"/>
+
+				<Link href='/'>
+					<Image src={logo} alt="ig.news logo"/>
+				</Link>
 
 				<nav>
 					<ActiveLink activeClassName={styles.active} href='/'>
