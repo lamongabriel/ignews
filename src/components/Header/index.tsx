c0577ffb1d61bf5015462f39c35d9cc1dbab92dd
lamongabriel <gabriel-lamon@outlook.com>
2023-01-12 16:54:22 -0300

    header responsive and home responsive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,30 +4,51 @@ import Image from 'next/image'
 import { SignInButton } from '../SignInButton'
 import { ActiveLink } from '../ActiveLink'
 
+import { FaBars } from 'react-icons/fa'
+
 import styles from './styles.module.scss'
 
 import logo from '../../assets/logo.svg'
+import { useEffect, useState } from 'react'
 
 export function Header () {
 
+	const [menuOpen, setMenuOpen] = useState(false)
+
+	function toggleMobMenu () {
+		setMenuOpen(prev => !prev)
+	}
+
+	useEffect(() => {
+		if(menuOpen){
+			document.body.style.overflow = 'hidden'
+			return
+		}
+		document.body.style.overflow = 'auto'
+	}, [menuOpen])
+
 	return (
 		<header className={styles.headerContainer}>
-			<div className={styles.headerContent + ' container'}>
+			<div className={styles.headerContent}>
 
 				<Link href='/'>
 					<Image src={logo} alt="ig.news logo"/>
 				</Link>
 
-				<nav>
+				<button className={styles.menuButton} onClick={toggleMobMenu}>
+					<FaBars size={22} color="#e1e1e6" />
+				</button>
+
+				<nav className={menuOpen ? styles.navOpen : ''}>
 					<ActiveLink activeClassName={styles.active} href='/'>
-						Home
+								Home
 					</ActiveLink>
 					<ActiveLink activeClassName={styles.active} href='/posts'>
-						Posts
+								Posts
 					</ActiveLink>
+					<SignInButton />
 				</nav>
 
-				<SignInButton />
 			</div>
 		</header>
 	)
